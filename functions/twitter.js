a424d4295524267655c02e0cc2e1d267189c9ca4
Ghirtor <aymeric.debuisson@gmail.com>
2018-05-03 11:37:36 +0200

    added comments

--- functions/twitter.js
+++ functions/twitter.js
@@ -7,11 +7,13 @@ exports.defaultLogin = 'Ghirtor';
 var followersCount = 0;
 var lastTweetContent = '';
 
+// post a tweet containing message 
 exports.postTweet = function (message) {
 	twitterApi.post("statuses/update",{status: message}, function (err,data,res) {
 	});
 }
 
+// get the number of followers
 exports.getFollowersCount = function (login) {
 	twitterApi.get('followers/ids', {screen_name: login}, function (err, data, response) {
 		followersCount = data.ids.length;
@@ -19,12 +21,14 @@ exports.getFollowersCount = function (login) {
 	return followersCount;
 }
 
+// do an action with the last tweet
 exports.getLastTweet = function (login, callback) {
 	var params = {screen_name: login, count: 1};
 	twitterApi.get('statuses/user_timeline', params, callback);
 	return lastTweetContent;
 }
 
+// suppress last tweet
 exports.suppressLastTweet = function (err, data, response) {
 	if (data.length > 0) {
 		var params = {id: data[0].id_str};
@@ -34,6 +38,7 @@ exports.suppressLastTweet = function (err, data, response) {
 	}
 }
 
+// read last tweet
 exports.readLastTweet = function (err, data, response) {
 	if (data.length > 0) {
 		lastTweetContent = data[0].text;
